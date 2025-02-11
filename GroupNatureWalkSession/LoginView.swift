d5a298c258f3ed2227fad180c72ae230cbb85a17
WayneWang1999 <106826164+WayneWang1999@users.noreply.github.com>
2025-02-10 22:25:43 -0500

    Update LoginView.swift

--- GroupNatureWalkSession/LoginView.swift
+++ GroupNatureWalkSession/LoginView.swift
@@ -1,33 +1,69 @@
-//
-//  LoginView.swift
-//  GroupNatureWalkSession
-//
-//  Created by rg on 2/6/25.
-//
-
 import SwiftUI
 
 struct LoginView: View {
     @Binding var isLoggedIn: Bool
-    
-    
+    @State private var username: String = UserDefaults.standard.string(forKey: "savedUsername") ?? ""
+    @State private var password: String = UserDefaults.standard.string(forKey: "savedPassword") ?? ""
+    @State private var rememberMe: Bool = UserDefaults.standard.bool(forKey: "rememberMe")
+    @State private var errorMessage: String?
+
+    // List of valid usernames and passwords
+    private let validUsers: [String: String] = [
+        "admin": "111111",
+        "user1": "111111",
+        "user2": "111111"
+    ]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        
-        Button("Login") {
-            isLoggedIn = true
+        VStack {
+            Text("Login")
+                .font(.largeTitle)
+                .padding()
+
+            TextField("Username", text: $username)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .autocapitalization(.none)
+                .padding()
+
+            SecureField("Password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+
+            Toggle("Remember Me", isOn: $rememberMe)
+                .padding()
+
+            if let errorMessage = errorMessage {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+                    .padding()
+            }
+
+            Button("Login") {
+                if let correctPassword = validUsers[username], correctPassword == password {
+                    // Save credentials if "Remember Me" is checked
+                    if rememberMe {
+                        UserDefaults.standard.set(username, forKey: "savedUsername")
+                        UserDefaults.standard.set(password, forKey: "savedPassword")
+                        UserDefaults.standard.set(true, forKey: "rememberMe")
+                    } else {
+                        UserDefaults.standard.removeObject(forKey: "savedUsername")
+                        UserDefaults.standard.removeObject(forKey: "savedPassword")
+                        UserDefaults.standard.set(false, forKey: "rememberMe")
+                    }
+                    isLoggedIn = true
+                } else {
+                    // Show error message for incorrect credentials
+                    errorMessage = "Invalid username or password. Please try again."
+                    password = "" // Clear password field for re-entry
+                }
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(8)
+            .padding()
         }
         .padding()
-        .background(Color.blue)
-        .foregroundColor(.white)
-        .cornerRadius(8)
-            
-        
     }
 }
-
-//struct LoginView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        LoginView()
-//    }
-//}
